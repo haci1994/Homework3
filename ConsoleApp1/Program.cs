1a58c96b8c8bdce19a6f36e33e3d6996c469ec44
haci1994 <65703054+haci1994@users.noreply.github.com>
2025-02-15 01:13:57 +0400

    1st task

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Homework homework = new Homework();
+
+
+            /* 1.*/
+            int[] numbers = [2,-5,199];
+
+            homework.GetMinMax(numbers, out int max, out int min);
+
+            Console.WriteLine($"max={max}, min = {min}");
         }
     }
-}
+
+        public class Homework { 
+
+            /* 1. Create a method named GetMinMax that takes
+                an array of integers and uses the out keyword
+                to return both the minimum and maximum values
+                in the array.*/
+
+            public void GetMinMax(int[] nums, out int max, out int min)
+            {
+                max = nums[0];
+                min = nums[0];
+
+                for (int i = 0; i < nums.Length; i++) {
+                    if (nums[i] > max) { 
+                    max = nums[i];
+                    }
+
+                    if (nums[i] < min) {
+                    min = nums[i];
+                    }
+                }
+                                
+            }
+        }
+    }
+
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2e98194fbe56fd0ac340aee60a13c65c3cbe41f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
