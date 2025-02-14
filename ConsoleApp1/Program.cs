a06b9a6b705a905d484a005f3c084f73e914e845
haci1994 <65703054+haci1994@users.noreply.github.com>
2025-02-15 01:53:20 +0400

    3rd task

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ConsoleApp1
+﻿using System.ComponentModel;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
@@ -8,6 +10,7 @@
 
             int[] numbers = [2, -5, 199];
             double[] nums = [1, 3, 5, 7];
+            DateTime birthDate = new DateTime(1994, 6, 5);
 
             /* 1.*/
             homework.GetMinMax(out int max, out int min, numbers);
@@ -15,6 +18,9 @@
 
             /* 2.*/
             Console.WriteLine($"Average is {homework.AverageNumbers(nums)}");
+
+            /* 3. */
+            Console.WriteLine($"Your living days count is - {homework.CalculateAgeInDays(birthDate)}");
 
         }
     }
@@ -69,7 +75,23 @@
 
                 return sum / nums.Length;
             }
-
         }
-    }
+
+
+        /* 3. Research about DateTime and TimeSpan types and solve this.
+        Write a method called CalculateAgeInDays that takes a DateTime parameter
+        representing a person's birthdate and returns their age. Use the
+        current date (DateTime.Now) to calculate the difference.*/
+
+        public int CalculateAgeInDays (DateTime birth)  {
+
+            DateTime today = DateTime.Now;
+
+            TimeSpan ageDays = today - birth;
+            
+            return ageDays.Days;
+
+            }
+
+}
 }
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a58c96b8c8bdce19a6f36e33e3d6996c469ec44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e5c17f8b8c9eefd3ca8a71652196e62e0d40b49")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
